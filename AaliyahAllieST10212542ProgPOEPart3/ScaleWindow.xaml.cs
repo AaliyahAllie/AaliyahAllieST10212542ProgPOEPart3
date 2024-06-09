bdc2582e1666d3f3eb94b6612cd1e595f7e5e6aa
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-06-09 22:57:36 +0200

    LOGIC FOR SCALE WINDOW ADDED

--- AaliyahAllieST10212542ProgPOEPart3/ScaleWindow.xaml.cs
+++ AaliyahAllieST10212542ProgPOEPart3/ScaleWindow.xaml.cs
@@ -1,27 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using AaliyahAllieST10212542ProgPOEPart3;
 
 namespace AaliyahAllieST10212542ProgPOEPart3
 {
-    /// <summary>
-    /// Interaction logic for ScaleWindow.xaml
-    /// </summary>
     public partial class ScaleWindow : Window
     {
-        public ScaleWindow()
+        private List<Recipe> _recipes;
+
+        public ScaleWindow(List<Recipe> recipes)
         {
             InitializeComponent();
+            _recipes = recipes;
+            PopulateRecipeNames();
+        }
+
+        private void PopulateRecipeNames()
+        {
+            // Clear existing items
+            RecipeComboBox.Items.Clear();
+
+            // Populate the ComboBox with recipe names
+            foreach (var recipe in _recipes)
+            {
+                RecipeComboBox.Items.Add(recipe.RecipeName);
+            }
+        }
+
+        private void Scale_Click(object sender, RoutedEventArgs e)
+        {
+            if (RecipeComboBox.SelectedItem is string selectedRecipeName)
+            {
+                Recipe selectedRecipe = _recipes.Find(recipe => recipe.RecipeName == selectedRecipeName);
+                if (selectedRecipe != null)
+                {
+                    // Scale the selected recipe
+                    if (ScaleFactorComboBox.SelectedItem is ComboBoxItem selectedItem)
+                    {
+                        double factor = double.Parse(selectedItem.Content.ToString());
+                        selectedRecipe.ScaleRecipe(factor);
+                        MessageBox.Show($"Recipe '{selectedRecipeName}' scaled by a factor of {factor}");
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select a scaling factor.");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a recipe to scale.");
+            }
         }
     }
 }
