d3634f3c9c994f19da93622d0f1c86f0be336750
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-06-09 22:55:59 +0200

    RESET RECIPE FUNCTIONS ADDED (THEY DO NOT WORK FULLY WILL BE EDITED AT LATER STAGE OF DEVELOPMENT)

---
+++ AaliyahAllieST10212542ProgPOEPart3/ResetWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using AaliyahAllieST10212542ProgPOEPart3;
+
+namespace AaliyahAllieST10212542ProgPOEPart3
+{
+    public partial class ResetWindow : Window
+    {
+        private List<Recipe> _recipes;
+
+        public ResetWindow(List<Recipe> recipes)
+        {
+            InitializeComponent();
+            _recipes = recipes;
+            PopulateRecipeNames();
+        }
+
+        private void PopulateRecipeNames()
+        {
+            // Clear existing items
+            RecipeComboBox.Items.Clear();
+
+            // Populate the ComboBox with recipe names
+            foreach (var recipe in _recipes)
+            {
+                RecipeComboBox.Items.Add(recipe.RecipeName);
+            }
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (RecipeComboBox.SelectedItem is string selectedRecipeName)
+            {
+                Recipe selectedRecipe = _recipes.Find(recipe => recipe.RecipeName == selectedRecipeName);
+                if (selectedRecipe != null)
+                {
+                    // Reset the selected recipe
+                    selectedRecipe.ResetRecipe();
+                    MessageBox.Show($"Recipe '{selectedRecipeName}' has been reset.");
+                    this.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a recipe to reset.");
+            }
+        }
+    }
+}
