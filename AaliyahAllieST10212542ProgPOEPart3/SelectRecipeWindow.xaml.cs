4a4acca66f130b993bc4a52b073bbb23402facba
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-06-09 22:59:53 +0200

    Select recipe created for display

---
+++ AaliyahAllieST10212542ProgPOEPart3/SelectRecipeWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using AaliyahAllieST10212542ProgPOEPart3;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace AaliyahAllieST10212542ProgPOEPart3
+{
+    public partial class SelectRecipeWindow : Window
+    {
+        // Property to hold the selected recipe name
+        public string SelectedRecipeName { get; private set; }
+
+        public SelectRecipeWindow(List<Recipe> recipes)
+        {
+            InitializeComponent();
+
+            // Populate the ComboBox with recipe names
+            foreach (Recipe recipe in recipes)
+            {
+                RecipeComboBox.Items.Add(recipe.RecipeName);
+            }
+
+            // Select the first item by default
+            if (RecipeComboBox.Items.Count > 0)
+            {
+                RecipeComboBox.SelectedIndex = 0;
+            }
+        }
+
+        // Event handler for the "Display" button click
+        private void DisplayButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the selected recipe name
+            SelectedRecipeName = RecipeComboBox.SelectedItem as string;
+
+            // Close the window
+            DialogResult = true;
+        }
+    }
+}
